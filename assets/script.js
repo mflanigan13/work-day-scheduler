9b2d774f7ab16054ecf2cab32aff522ddfeb10ce
mflanigan <mflanigantwualumn@gmail.com>
2021-12-13 17:56:40 -0600

    linked to correct js files, added date at top of website

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
-console.log(moment());
+// current day is displayed at top of the calendar
+var today = moment().format('LLLL');
+$("#currentDay").html(today);
+
+// color coded time blocks
+
+
+// enter event for time block
+
+// save button 
+
+// save events in local storage
+
+// refresh the page, the events persists
